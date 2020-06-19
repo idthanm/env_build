ec91a4a1c4aafe103a03ef3f385968f8aa4f649f
guany17@mails.tsinghua.edu.cn <guany17@mail.tsinghua.edu.cn>
2020-06-19 21:38:52 +0800

    change good done reward, change reward coeffi

--- dynamics_and_models.py
+++ dynamics_and_models.py
@@ -187,7 +187,7 @@ class EnvironmentModel(object):  # all tensors
         dones_reward = tf.where(dones_type=='break_road_constrain', -20., dones_reward)
         dones_reward = tf.where(dones_type=='deviation_too_much', -20., dones_reward)
         dones_reward = tf.where(dones_type=='break_stability', -20., dones_reward)
-        dones_reward = tf.where(dones_type=='good_done', 20., dones_reward)
+        dones_reward = tf.where(dones_type=='good_done', 50., dones_reward)
         this_step_dones = tf.cast(dones, tf.float32) - tf.cast(prev_dones, tf.float32)
         dones_reward = dones_reward * this_step_dones
         return dones_reward
@@ -805,13 +805,13 @@ class EnvironmentModel(object):  # all tensors
                         veh2veh_dist = tf.sqrt(
                             tf.square(ego_point[0] - veh_point[0]) + tf.square(ego_point[1] - veh_point[1])) - \
                                        tf.convert_to_tensor(rho_ego + rho_vehs, dtype=tf.float32)
-                        # veh2veh -= 1 / tf.square(veh2veh_dist)
-                        veh2veh -= tf.nn.relu(-(veh2veh_dist - 10.))
+                        veh2veh -= 1 / veh2veh_dist
+                        # veh2veh -= tf.nn.relu(-(veh2veh_dist - 10.))
 
-            # veh2veh = tf.where(veh2veh < -10., -10. * tf.ones_like(veh2veh), veh2veh)
+            veh2veh = tf.where(veh2veh < -10., -10. * tf.ones_like(veh2veh), veh2veh)
 
-            rewards = 0.01 * devi_v + 0.04 * devi_y + 5 * devi_phi + 0.02 * punish_yaw_rate + \
-                      0.05 * punish_steer + 0.0005 * punish_a_x + 0.05*veh2veh + 0.05*veh2road
+            rewards = 0.01 * devi_v + 0.1 * devi_y + 5 * devi_phi + 0.02 * punish_yaw_rate + \
+                      0.05 * punish_steer + 0.0005 * punish_a_x + veh2veh + 0.02*veh2road
             rewards = tf.cast(tf.math.logical_not(prev_dones), tf.float32) * rewards
             # self.reward_info = dict(punish_steer=punish_steer.numpy()[0],
             #                         punish_a_x=punish_a_x.numpy()[0],
@@ -828,10 +828,10 @@ class EnvironmentModel(object):  # all tensors
             #                         scaled_punish_a_x=0.0005 * punish_a_x.numpy()[0],
             #                         scaled_punish_yaw_rate=0.02 * punish_yaw_rate.numpy()[0],
             #                         scaled_devi_v=0.01 * devi_v.numpy()[0],
-            #                         scaled_devi_y=0.04 * devi_y.numpy()[0],
+            #                         scaled_devi_y=0.1 * devi_y.numpy()[0],
             #                         scaled_devi_phi=5 * devi_phi.numpy()[0],
-            #                         scaled_veh2road=0.05*veh2road.numpy()[0],
-            #                         scaled_veh2veh=0.05*veh2veh.numpy()[0],
+            #                         scaled_veh2road=0.02*veh2road.numpy()[0],
+            #                         scaled_veh2veh=veh2veh.numpy()[0],
             #                         scaled_rew_alpha_f=0.,
             #                         scaled_rew_alpha_r=0.,
             #                         scaled_rew_r=0.,
