d3b8bffa50623af1365aeb4c9a59b0857ac308a4
guany17@mails.tsinghua.edu.cn <guany17@mail.tsinghua.edu.cn>
2020-09-04 22:20:33 +0800

    change devi_v devi_y coeffi

--- endtoend.py
+++ endtoend.py
@@ -578,7 +578,7 @@ class CrossroadEnd2end(gym.Env):
         #             veh2veh -= 1. / tf.abs(veh2veh_dist)
                     # veh2veh -= tf.nn.relu(-(veh2veh_dist-10.))
 
-        reward = 0.04 * devi_v + 0.04 * devi_y + 0.1 * devi_phi + 0.02 * punish_yaw_rate + \
+        reward = 0.01 * devi_v + 0.01 * devi_y + 0.1 * devi_phi + 0.02 * punish_yaw_rate + \
                   2. * punish_steer + 0.0005 * punish_a_x + 0.5 * veh2veh
         reward_dict = dict(punish_steer=punish_steer.numpy(),
                            punish_a_x=punish_a_x.numpy(),
@@ -590,8 +590,8 @@ class CrossroadEnd2end(gym.Env):
                            scaled_punish_steer=2. * punish_steer.numpy(),
                            scaled_punish_a_x=0.0005 * punish_a_x.numpy(),
                            scaled_punish_yaw_rate=0.02 * punish_yaw_rate.numpy(),
-                           scaled_devi_v=0.04 * devi_v.numpy(),
-                           scaled_devi_y=0.04 * devi_y.numpy(),
+                           scaled_devi_v=0.01 * devi_v.numpy(),
+                           scaled_devi_y=0.01 * devi_y.numpy(),
                            scaled_devi_phi=0.1 * devi_phi.numpy(),
                            scaled_veh2veh=0.5 * veh2veh.numpy(),)
         return reward.numpy(), reward_dict
