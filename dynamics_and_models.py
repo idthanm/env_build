ef688caf8de5779b54bb20c0ec7c87c5704759c3
guany17@mails.tsinghua.edu.cn <guany17@mail.tsinghua.edu.cn>
2020-09-05 00:28:20 +0800

    modify coeffi;a slight good version

--- dynamics_and_models.py
+++ dynamics_and_models.py
@@ -207,7 +207,7 @@ class EnvironmentModel(object):  # all tensors
             #             veh2veh -= 1 / tf.abs(veh2veh_dist)
             #             # veh2veh -= tf.nn.relu(-(veh2veh_dist - 10.))
             #
-            rewards = 0.01 * devi_v + 0.01 * devi_y + 0.1 * devi_phi + 0.02 * punish_yaw_rate + \
+            rewards = 0.04 * devi_v + 0.01 * devi_y + 0.1 * devi_phi + 0.02 * punish_yaw_rate + \
                       2. * punish_steer + 0.0005 * punish_a_x + 0.5 * veh2veh
             # self.reward_info = dict(punish_steer=punish_steer.numpy()[0],
             #                         punish_a_x=punish_a_x.numpy()[0],
@@ -219,7 +219,7 @@ class EnvironmentModel(object):  # all tensors
             #                         scaled_punish_steer=2. * punish_steer.numpy()[0],
             #                         scaled_punish_a_x=0.0005 * punish_a_x.numpy()[0],
             #                         scaled_punish_yaw_rate=0.02 * punish_yaw_rate.numpy()[0],
-            #                         scaled_devi_v=0.01 * devi_v.numpy()[0],
+            #                         scaled_devi_v=0.04 * devi_v.numpy()[0],
             #                         scaled_devi_y=0.01 * devi_y.numpy()[0],
             #                         scaled_devi_phi=0.1 * devi_phi.numpy()[0],
             #                         scaled_veh2veh=0.5 * veh2veh.numpy()[0],
