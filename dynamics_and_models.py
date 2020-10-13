500caac0bce7591fbf5ebdb21a0e1d65c502785b
guany17@mails.tsinghua.edu.cn <guany17@mail.tsinghua.edu.cn>
2020-10-13 19:25:23 +0800

    tracking the current point with future phis

--- dynamics_and_models.py
+++ dynamics_and_models.py
@@ -713,10 +713,10 @@ class ReferencePath(object):
         return points[0], points[1], points[2]
 
     def tracking_error_vector(self, ego_xs, ego_ys, ego_phis, ego_vs, n):
-        indexs, _ = self.find_closest_point(ego_xs, ego_ys)
-        tracking_points = self.indexs2points(indexs+30)
-        n_future_data = self.future_n_data(indexs+30, n)
-        all_ref = [tracking_points] + n_future_data
+        indexs, current_points = self.find_closest_point(ego_xs, ego_ys)
+        tracking_points = self.indexs2points(indexs+50)
+        n_future_data = self.future_n_data(indexs+50, n)
+        all_ref = [(current_points[0], current_points[1], tracking_points[2])] + n_future_data
 
         def two2one(ref_xs, ref_ys):
             if self.task == 'left':
