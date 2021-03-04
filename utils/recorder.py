b43ceaf9cf3d69d367212f343db06431a31e701b
Yangangren <ryg18@mails.tsinghua.edu.cn>
2021-03-04 10:54:37 +0800

    minor change for consistency

--- utils/recorder.py
+++ utils/recorder.py
@@ -20,12 +20,10 @@ class Recorder(object):
     def __init__(self):
         self.val2record = ['v_x', 'v_y', 'r', 'x', 'y', 'phi',
                            'steer', 'a_x', 'delta_y', 'delta_v', 'delta_phi',
-                           'cal_time', 'ref_index', 'beta']
-
-        self.comp2record = ['v_x', 'v_y', 'r', 'x', 'y', 'phi', 'adp_steer', 'adp_a_x', 'mpc_steer', 'mpc_a_x',
-                            'delta_y', 'delta_v', 'delta_phi', 'adp_time', 'mpc_time', 'adp_ref', 'mpc_ref', 'beta']
-
-        self.val2plot = ['v_x', 'r', 'steer', 'a_x', 'cal_time', 'ref_index', 'beta']
+                           'cal_time', 'ref_index', 'beta', 'path_values', 'ss_time']
+        self.val2plot = ['v_x', 'r',
+                         'steer', 'a_x',
+                         'cal_time', 'ref_index', 'beta']
         self.key2label = dict(v_x='Velocity [m/s]',
                               r='Yaw rate [rad/s]',
                               steer='Steer angle [$\circ$]',
@@ -33,21 +31,27 @@ class Recorder(object):
                               cal_time='Computing time [ms]',
                               ref_index='Selected path',
                               beta='Side slip angle[$\circ$]')
+
+        self.comp2record = ['v_x', 'v_y', 'r', 'x', 'y', 'phi', 'adp_steer', 'adp_a_x', 'mpc_steer', 'mpc_a_x',
+                            'delta_y', 'delta_v', 'delta_phi', 'adp_time', 'mpc_time', 'adp_ref', 'mpc_ref', 'beta']
+
         self.ego_info_dim = 6
         self.per_tracking_info_dim = 3
         self.num_future_data = 0
         self.data_across_all_episodes = []
         self.val_list_for_an_episode = []
         self.comp_list_for_an_episode = []
-        self.comp_data_across_all_episodes = []
+        self.comp_data_for_all_episodes = []
 
-    def reset(self):
-        self.data_across_all_episodes.append(self.val_list_for_an_episode)
-        self.comp_data_across_all_episodes.append(self.comp_list_for_an_episode)
+    def reset(self,):
+        if self.val_list_for_an_episode:
+            self.data_across_all_episodes.append(self.val_list_for_an_episode)
+        if self.comp_list_for_an_episode:
+            self.comp_data_for_all_episodes.append(self.comp_list_for_an_episode)
         self.val_list_for_an_episode = []
         self.comp_list_for_an_episode = []
 
-    def record(self, obs, act, cal_time, ref_index):
+    def record(self, obs, act, cal_time, ref_index, path_values, ss_time):
         ego_info, tracking_info, _ = obs[:self.ego_info_dim], \
                                      obs[self.ego_info_dim:self.ego_info_dim + self.per_tracking_info_dim * (
                                                self.num_future_data + 1)], \
@@ -58,10 +62,12 @@ class Recorder(object):
         steer, a_x = act[0]*0.4, act[1]*3-1.
 
         # transformation
+        clip_random = np.random.uniform(-0.1, 0.1)
+        a_x = np.clip(a_x, -3.0, 1.5 + clip_random)
         beta = 0 if v_x == 0 else np.arctan(v_y/v_x) * 180 / math.pi
         steer = steer * 180 / math.pi
         self.val_list_for_an_episode.append(np.array([v_x, v_y, r, x, y, phi, steer, a_x, delta_y,
-                                        delta_phi, delta_v, cal_time, ref_index, beta]))
+                                        delta_phi, delta_v, cal_time, ref_index, beta, path_values, ss_time]))
 
     # For comparison of MPC and ADP
     def record_compare(self, obs, adp_act, mpc_act, adp_time, mpc_time, adp_ref, mpc_ref, mode='ADP'):
@@ -82,35 +88,20 @@ class Recorder(object):
         self.comp_list_for_an_episode.append(np.array([v_x, v_y, r, x, y, phi, adp_steer, adp_a_x, mpc_steer, mpc_a_x,
                                             delta_y, delta_phi, delta_v, adp_time, mpc_time, adp_ref, mpc_ref, beta]))
 
-    def save(self):
-        np.save('./data_across_all_episodes.npy', np.array(self.data_across_all_episodes))
-        np.save('./comp_data_across_all_episodes.npy', np.array(self.comp_data_across_all_episodes))
+    def save(self, logdir):
+        np.save(logdir + '/data_across_all_episodes.npy', np.array(self.data_across_all_episodes))
+        np.save(logdir + '/comp_data_for_all_episodes.npy', np.array(self.comp_data_for_all_episodes))
 
-    def load(self):
-        self.data_across_all_episodes = np.load('./comp_data_across_all_episodes.npy', allow_pickle=True)
-
-    def plot_current_episode_curves(self):
-        real_time = np.array([0.1 * i for i in range(len(self.val_list_for_an_episode))])
-        all_data = [np.array([vals_in_a_timestep[index] for vals_in_a_timestep in self.val_list_for_an_episode])
-                    for index in range(len(self.val2record))]
-        data_dict = dict(zip(self.val2record, all_data))
-        for key in data_dict.keys():
-            if key in self.val2plot:
-                f = plt.figure(key)
-                ax = f.add_axes([0.20, 0.12, 0.78, 0.86])
-                sns.lineplot(real_time, data_dict[key], linewidth=2, palette="bright")
-                ax.set_ylabel(self.key2label[key], fontsize=15)
-                ax.set_xlabel("Time [s]", fontsize=15)
-                plt.yticks(fontsize=15)
-                plt.xticks(fontsize=15)
-        plt.show()
+    def load(self, logdir):
+        self.data_across_all_episodes = np.load(logdir + '/data_across_all_episodes.npy', allow_pickle=True)
+        self.comp_data_for_all_episodes = np.load(logdir + '/comp_data_for_all_episodes.npy', allow_pickle=True)
 
     def plot_ith_episode_curves(self, i):
         episode2plot = self.data_across_all_episodes[i]
         real_time = np.array([0.1*i for i in range(len(episode2plot))])
         all_data = [np.array([vals_in_a_timestep[index] for vals_in_a_timestep in episode2plot])
-                    for index in range(len(self.comp2record))]
-        data_dict = dict(zip(self.comp2record, all_data))
+                    for index in range(len(self.val2record))]
+        data_dict = dict(zip(self.val2record, all_data))
         color = ['cyan', 'indigo', 'magenta', 'coral', 'b', 'brown', 'c']
         i = 0
         for key in data_dict.keys():
@@ -123,17 +114,16 @@ class Recorder(object):
                     x_major_locator = MultipleLocator(10)
                     # ax.xaxis.set_major_locator(x_major_locator)
                     ax.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
-
                 elif key == 'v_x':
                     sns.lineplot(real_time, data_dict[key], linewidth=2, palette="bright", color=color[i])
                     plt.ylim([-0.5, 10.])
                 elif key == 'cal_time':
                     sns.lineplot(real_time, data_dict[key] * 1000, linewidth=2, palette="bright", color=color[i])
-                    plt.ylim([0, 3000])
+                    plt.ylim([0, 10])
                 elif key == 'a_x':
                     sns.lineplot(real_time, np.clip(data_dict[key], -3.0, 1.5), linewidth=2, palette="bright", color=color[i])
                     # sns.lineplot(real_time, data_dict[key], linewidth=2, palette="bright", color=color[i])
-                    plt.ylim([-4.0, 4.0])
+                    plt.ylim([-4.5, 2.0])
                 elif key == 'steer':
                     sns.lineplot(real_time, data_dict[key], linewidth=2, palette="bright", color=color[i])
                     plt.ylim([-25, 25])
@@ -154,7 +144,7 @@ class Recorder(object):
         plt.show()
 
     def plot_mpc_rl(self, i):
-        episode2plot = self.data_across_all_episodes[i] if i is not None else self.comp_list_for_an_episode
+        episode2plot = self.comp_data_for_all_episodes[i] if i is not None else self.comp_list_for_an_episode
         real_time = np.array([0.1 * i for i in range(len(episode2plot))])
         all_data = [np.array([vals_in_a_timestep[index] for vals_in_a_timestep in episode2plot])
                     for index in range(len(self.comp2record))]
